5ca1b6d4551ed56dcedacd024023ccf2d5c6abf1
Eduardo <eduardoorbenesdiaz@gmail.com>
2017-07-10 18:44:41 -1200

    cambios pruebas

--- views/admin/cajeros-panel.php
+++ views/admin/cajeros-panel.php
@@ -1,50 +1,53 @@
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link href="../../materialize/css/materialize.css" rel="stylesheet" type="text/css"/>
-        <script src="../../materialize/js/jquery.js" type="text/javascript"></script>
-        <script src="../../materialize/js/materialize.js" type="text/javascript"></script>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <script src="https://code.highcharts.com/stock/highstock.js"></script>
-        <script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
-        <link href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css"/>
-        <script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
-        <link href="../../util/css/font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet" type="text/css"/>
-                <link rel="icon" type="image/png" href="https://s-media-cache-ak0.pinimg.com/originals/9d/63/bd/9d63bd96356d4ac066ee53aa699250e9.png" />
+        <?php include '../../util/html-generic/head-links-and-scripts.php'; ?>
         <title>Panel ADM V 1.0</title>
     </head>
     <body>
         <?php
-        include '../../util/usuarios/getUsuarios.php';
-
-        if (isset($_POST["nombre"])) {
-            $usuario = new Usuario();
-            $nombre = $_POST["nombre"];
-            $correo = $_POST["email"];
-            $telefono = $_POST["telefono"];
-            $actualizarUsuario = $usuario->__actualizarUsuario($nombre, $correo, $telefono);
-            echo "<script>alert(actualizado);</script>";
-        }
-        ?>  
-        <script>
-            $(document).ready(function () {
-                $('#table-users').DataTable();
-                $("select").val('10'); //seleccionar valor por defecto del select
-                $('select').addClass("browser-default"); //agregar una clase de materializecss de esta forma ya no se pierde el select de numero de registros.
-                $('select').material_select(); //inicializar el select de materialize
-                $('ul.tabs').tabs('select_tab', 'tab_id');
-                $('.modal').modal();
-                $('.modal2').modal();
-            });
+        //obtenemos los objetos que contienen usuarios-cajeros
+        include '../../controllers/usuarios/getUsuarios.php';
+        ?>
+        <script type="text/javascript">
+        $(document).ready(function () {
+            $(".button-collapse").sideNav();
+            $('#table-users').DataTable();
+            $("select").val('10'); //seleccionar valor por defecto del select
+            $('select').addClass("browser-default"); //agregar una clase de materializecss de esta forma ya no se pierde el select de numero de registros.
+            $('select').material_select(); //inicializar el select de materialize
+            $('ul.tabs').tabs('select_tab', 'tab_id');
+            $('.modal').modal();
+            <?php
+              //iniciamos el modal de view
+              foreach ($cajeros as $value) {
+                  echo "$(." . $value['usuario_id'] . ").modal();";
+                  echo "$(." . $value['telefono'] . ").modal();";
+              }
+      
+            ?>
+            $('#modal-agregar').modal();
+        });
         </script>
         <!-- Header -->
-        <?php include '../../util/html-admin/header.php';?>
+        <?php include './header.php'; ?>
         <!-- Content -->
+        <style media="screen">
+            #btn-agregar{
+                position: relative;
+                right: 30px;
+                bottom: 30px;
+            }
+
+            #btn-agregar{
+                position: fixed;
+                right: 30px;
+                bottom: 30px;
+            }
+        </style>
         <div class="row">
             <div class="col m2"></div>
             <div class="col s12 m9">
-
-                <?php foreach ($usuarios as $row) { ?>        
+                <?php foreach ($usuarios as $row) { ?>
                     <div class="card col m3 s12">
                         <div class="card-image">
                             <img src="../../img/3.jpg" alt=""/>
@@ -67,81 +70,128 @@
                     </div>
                 <?php } ?>
             </div><br>
-            <a class="btn-floating btn-large waves-effect darken-2"><i class="material-icons">add</i></a>
+            <div id="modal-add" class="modal">
+                <?php include '../../controllers/cajeros/add_cajeros.php'; ?>
+            </div>
         </div>
         <div class="row">
             <div class="col m2"></div>
             <div class="col m10">
-                <table id="table-users">
-                    <thead>
-                        <tr>
-                            <th>Nombre</th>
-                            <th>Teléfono</th>
-                            <th>Correo</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>               
-                        <?php foreach ($usuarios as $row) {
-                            ?>
-                            <tr>
-                                <td><?php echo $row["nombre_completo"]; ?></td>
-                                <td><?php echo $row["telefono"]; ?></td>
-                                <td><?php echo $row["correo"]; ?></td>
-                                <td> <a class="btn-flat waves-effect " href="#modal1"><i class="fa fa-eye" aria-hidden="true"></i></a>
-                                    <!-- Modal Structure -->
-                                    <div id="modal1" class="modal">
-                                        <div class="modal-content">
-                                            <div class="row valign-wrapper">
-                                                <div class="col s2">
-                                                    <img src="<?php echo "../../" . $row["img_url_perfil"]; ?>" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
-                                                </div>
-                                                <div class="col s10">
-                                                    <span class="black-text">
-                                                        <?php echo $row["nombre_completo"]; ?><hr><br>
-                                                        <?php echo $row["correo"]; ?><br>
-                                                        <?php echo $row["telefono"]; ?>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                        </div>
-
+                <a id="btn-agregar" class="btn-floating btn-large waves-effect waves-light red" href="#modal-agregar"><i class="material-icons">add</i></a>
+                <!-- Modal Structure -->
+                <div id="modal-agregar" class="modal">
+                    <div class="modal-content">
+                        <div class="row">
+                            <form action="../../controllers/cajeros/ajaxsubmit.php" method="post"class="col s12">
+                                <div class="row">
+                                    <div class="input-field col s6">
+                                        <input placeholder="Nombre" id="nombre" name="nombre" type="text" class="validate" required>
+                                        <label for="nombre">Nombre Completo</label>
                                     </div>
-                                    <a class="btn-flat small" href="path/to/settings" aria-label="Delete">
-                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
-                                    </a>
-                                    <a class="btn-flat waves-effect " href="#modal2"><i class="fa fa-pencil" aria-hidden="true"></i></a>
-                                    <div id="modal2" class="modal">
-                                        <div class="modal-content">
-                                            <div class="row">
-                                                <form method="POST" action="#" class="col s12">
-                                                    <div class="row">
-                                                        <div class="input-field col s6">
-                                                            <input placeholder="Placeholder" id="nombre" name="nombre" type="text" value="<?php echo $row["nombre_completo"]; ?>" class="validate">
-                                                            <label for="nombre">Nombre</label>
-                                                        </div>
-                                                        <div class="input-field col s6">
-                                                            <input id="telefono" name="telefono" type="text" class="validate" value="<?php echo $row["telefono"]; ?>">
-                                                            <label for="telefono">Telefono</label>
+                                    <div class="input-field col s6">
+                                        <input id="telefono" name="telefono" type="text" class="validate" required>
+                                        <label for="telefono">Numero de telefono</label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s6">
+                                        <input id="correo" type="email" name="correo" class="validate" required>
+                                        <label for="correo">Email</label>
+                                    </div>
+                                    <div class="input-field col s6">
+                                        <input id="password" name="password" type="password" class="validate" required>
+                                        <label for="password">Password de Sistema</label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <input class="btn" type="submit" name="submit" value="Enviar">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-content">
+                        <h4>Cajeros</h4>
+                        <table id="table-users">
+                            <thead>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Teléfono</th>
+                                    <th>Correo</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($cajeros as $row) {
+                                    ?>
+                                    <tr>
+                                        <td><?= $row["nombre_completo"] ?></td>
+                                        <td><?= $row["telefono"] ?></td>
+                                        <td><?= $row["correo"] ?></td>
+                                        <td> <a class="btn-flat waves-effect " href="#<?= $row["usuario_id"] ?>"><i class="fa fa-eye" aria-hidden="true"></i></a>
+                                            <!-- Modal Structure -->
+                                            <div id="<?= $row["usuario_id"] ?>" class="modal">
+                                                <div class="modal-content">
+                                                    <div class="row valign-wrapper">
+                                                        <div class="col s2">
+                                                          <?php
+                                                                if (!($row["img_url_perfil"] == "")) { ?>
+                                                                  <img src="<?= "../../img" . $row["img_url_perfil"] ?>" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                                                          <?php }else{ ?>
+                                                            <img src="../../img/logo.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                                                      <?php  }   ?>
+                                                                </div>
+                                                        <div class="col s10">
+                                                            <span class="black-text">
+                                                                <?= $row["nombre_completo"] ?><hr><br>
+                                                                <?= $row["correo"] ?><br>
+                                                                <?= $row["telefono"] ?>
+                                                            </span>
                                                         </div>
                                                     </div>
-                                                    <div class="row">
-                                                        <div class="input-field col s12">
-                                                            <input id="email" name="email" type="email" class="validate" value="<?php echo $row["correo"]; ?>">
-                                                            <label for="email">Email</label>
-                                                        </div>
-                                                    </div>
-                                                    <input class="waves-effect waves-light btn" type="submit" name="actualizar" value="Modificar"/>
-                                                </form>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
+                                            <a class="btn-flat small" href="../../controllers/cajeros/delete_cajeros.php?usuario=<?php echo $row["usuario_id"]; ?>" aria-label="Delete" onClick="javascript: return confirm('¿Confirmar Borrado de Cajero?');">
+                                                <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                            </a>
+                                            <a class="btn-flat waves-effect " href="#<?= $row["telefono"] ?>"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                                            <div id="<?= $row["telefono"] ?>" class="modal">
+                                                <div class="modal-content">
+                                                    <div class="row">
+                                                        <form action="../../controllers/cajeros/update.php" method="POST" action="#" class="col s12">
+                                                            <div class="row">
+                                                                <div class="input-field col s6">
+                                                                    <input placeholder="Placeholder" id="nombre" name="nombre" type="text" value="<?= $row["nombre_completo"]; ?>" class="validate">
+                                                                    <label for="nombre">Nombre</label>
+                                                                </div>
+                                                                <div class="input-field col s6">
+                                                                    <input id="telefono" name="telefono" type="text" class="validate" value="<?= $row["telefono"]; ?>">
+                                                                    <label for="telefono">Telefono</label>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="input-field col s12">
+                                                                    <input id="correo" name="correo" type="email" class="validate" value="<?= $row["correo"]; ?>">
+                                                                    <label for="correo">Email</label>
+                                                                    <input id="usuario_id" name="usuario_id" type="hidden" class="validate" value="<?= $row["usuario_id"]; ?>">
+                                                                </div>
+                                                            </div>
+                                                            <input class="waves-effect waves-light btn" type="submit" name="actualizar" value="Modificar"/>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-        </div> 
+        </div>
     </body>
 </html>
